97ecfbfc8f60f4c9f4257d21651b05710008ab9a
kaka_256 <kumarai460@gmail.com>
2024-09-26 12:42:49 +0900

    fix token input

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
+echo "input bot token"
+read token
+
+echo '{"token":"'$token'"}' > resource/token.json
+
 python -m venv .venv
 
 . .venv/bin/activate
 
 pip install discord.py
 pip install ping3
-
-read token
-
-echo '{"token":"'$token'"}' > resource/token.json
